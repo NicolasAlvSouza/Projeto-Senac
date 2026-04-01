6e61e292c7748958e6942c1cde8ad0d3b8dee5f7
NicolasAlvSouza <123672066+NicolasAlvSouza@users.noreply.github.com>
2026-04-01 00:31:51 +0000

    Introdução a Java script

---
+++ app.js
@@ -0,0 +1,13 @@
+const nome = 'Nicolas'
+let numTarefas = 1
+let tituloTarefa = 'Equaçoes Matematicas'
+let diciplina = 'Matematica'
+let desTarefa = 'Fazer equções de taefas'
+
+console.log (
+    `    Olá ${nome}, você tem ${numTarefas} taréfa pendente
+    Titulo: ${tituloTarefa} 
+    Diciplina: ${diciplina}
+    Descrição: ${desTarefa}
+    `)
+
